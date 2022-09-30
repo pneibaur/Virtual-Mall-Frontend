b9bd593475019bfa00c1fe54991161f7681091e0
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-30 12:40:04 -0600

    creates header/footer.jsx. adds scss file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There!</h1>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>HEADER NAVBAR</nav>
+  )
+}
+
+export default Header
