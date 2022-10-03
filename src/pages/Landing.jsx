df91416f559284d1c5c2016cf9b0e707e34fb243
Robert Ford <robert.ford1996@gmail.com>
2022-10-03 15:15:11 -0400

    install dom

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Store from './Store.jsx'
+import Cart from './Cart.jsx'
 
 const Landing = () => {
   return (
     <div className='landing'>
         <h1>Welcome to Virtual Mall</h1>
         <Store />
+        <Cart />
     </div>
   )
 }
