a9945c99ba72fe9fbe9d87cffab0f9c5e8007b33
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-10-04 14:12:59 -0600

    passes props to Home and maps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,28 @@
 
 import React from 'react'
-import { Link, Switch, Route } from "react-router-dom"
-import StoreIndex from './StoreIndex'
+import { Link } from "react-router-dom"
 
 const Store = (props) => {
     console.log("this is props ")
     console.log(props)
-    // this will be receiving props from app.js, and I can map out the different stores from here. 
+
     const loaded = () => {
-        return props.store.map((store) => {
-            <div className="storefront" key={store._id}>
-                <Link to={`/store/${store._id}/product/`}>
-                    <h2>{store.storeName}</h2>
-                    <img src={store.storeLogo} alt={store.storeName} />
-                </Link>
-            </div>
-        })
+        return props.store.map((store)=> (
+                <div key={store._id} className="storeFront">
+                    <h1>{store.storeName}</h1>
+                    <Link to={`/store/${store._id}/product`} >Go to store</Link>
+                </div>
+            ))
     }
+
     const loading = () => {
         return <h2>Loading...</h2>
     }
+
     return (
         <div>
-            <h1>Mall Index Page</h1>
-            {/* temporary link: will be deleted later */}
-            <Link to="/store/:id/product/">to store page</Link>
-            {/* below is commented out until app.js makes a successful api call. */}
-            {/* {props.store ? loaded() : loading()} */}
+            <h1>Stores List</h1>
+            {props.store ? loaded() : loading()}
         </div>
     )
 }
