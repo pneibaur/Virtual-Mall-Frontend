9fa2caecc18f01439bb847687e3d8d29e812a8f1
DeMarcus Brown <demarcus.m.brown@gmail.com>
2022-10-04 13:42:41 -0400

    updated home app and storeindex

--- src/pages/StoreIndex.jsx
+++ src/pages/StoreIndex.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { Link } from "react-router-dom"
 import ProductCard from '../components/ProductCard'
 
-const StoreIndex = () => {
+const StoreIndex = (props) => {
+  const [store, setStore] = useState(null)
+
+  // STORE DATABASE
+  const URL = "https://warm-fortress-13531.herokuapp.com/store/"
+
+  // Grabs store data from url
+  const getStore = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setStore(data);
+  };
   return (
     <div>
       <h2>Store Index page</h2>
       <ProductCard/>
-      <Link to="/home/:id/products/:id">Link to item details page</Link>
+      <Link to="/product/:id">Link to item details page</Link>
     </div>
 
   )
