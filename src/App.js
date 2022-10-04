0ba6357e441a47b19fe388dff53a3cead96c4eeb
Robert Ford <robert.ford1996@gmail.com>
2022-10-04 14:55:47 -0400

    api for app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Route, Switch } from "react-router-dom";
 import Cart from "./pages/Cart.jsx";
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Landing from './pages/Landing';
-import Home from "./pages/Home.jsx"
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home.jsx";
 import ProductsDisplayPage from "./pages/ProductsDisplayPage.jsx";
 import StoreIndex from "./pages/StoreIndex.jsx";
 
 function App() {
+  const [app, setApp] = useState(null);
+
+  const URL = "https://warm-fortress-13531.herokuapp.com";
+
+  const getApp = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setApp(data);
+  };
+
+  useEffect(() => {
+    getApp();
+  }, []);
+
   return (
     <div className="App">
       <h1>Virtual Mall</h1>
       <Header />
       <Switch>
         {/* Landing page */}
-        <Route exact path='/'>
+        <Route exact path="/">
           <Landing />
         </Route>
         {/* Cart page */}
         <Route path="/cart">
-          <Cart/>
+          <Cart />
         </Route>
         {/* "mallfront" home page */}
-        <Route path="/home" >
-          <Home />
+        <Route path="/home">
+          <Home store={app} />
         </Route>
         {/* store page */}
         <Route path="/store/:id/product/">
-          <StoreIndex/>
+          <StoreIndex />
         </Route>
         {/* product details page */}
         <Route path="/product/:id">
-          <ProductsDisplayPage/>
+          <ProductsDisplayPage />
         </Route>
       </Switch>
       <Footer />
